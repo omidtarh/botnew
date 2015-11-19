e26dd1cd388bad7f225cc4759d41d0ca72ac8ea1
omidtarh <omidtarh@att.net>
2015-11-19 21:23:42 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {V_for_Vendetta,140871556},
+    sudo_users = {omidtarh,70480064},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
